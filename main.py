9a24e09c0dea4d401b6854a102425198f64474e8
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2022-12-08 16:22:04 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+from art import logo
+
+print(logo)
+
+bids = {}
+
+def print_winner():
+  highest_bidder = {'name': 'none', 'bid': 0}
+  for bid in bids:
+    if bids[bid] > highest_bidder['bid']:
+      highest_bidder['name'] = bid
+      highest_bidder['bid'] = bids[bid]
+
+  print(f"The winner is {highest_bidder['name']} with a bid of     {highest_bidder['bid']}")
+
+def add_bid():
+  bidder_name = input("What is your name?")
+  bidder_bid = int(input("What is your bid?"))  
+  bids[bidder_name] = bidder_bid
+  bid_again = input("Any other bids?")
+  if bid_again == "Y":
+    add_bid()
+  else:
+    print(bids)
+    print_winner()
+add_bid()
+
+
